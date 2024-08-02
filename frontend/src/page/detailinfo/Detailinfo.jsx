9eaef4045dca6a06b2f4a55c6d485776a5bb8933
승진 <dydrkaan123@naver.com>
2024-08-02 19:55:49 +0900

    풀 받았습니다

--- frontend/src/page/detailinfo/Detailinfo.jsx
+++ frontend/src/page/detailinfo/Detailinfo.jsx
@@ -1,8 +1,52 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import {sendGet , URL } from '../../util/util'
+import './Detailinfo.scss'
 
 const Detailinfo = () => {
+
+    const [list , setList] = useState([])
+
+    const showConsole = (data) => {
+        console.log(data);
+        setList(data);
+    }
+
+    useEffect(()=>{
+        sendGet(URL + "/ppDetail" , showConsole)
+    },[])
+
   return (
-    <div>Detailinfo</div>
+        <div>
+        {list.map((item) => (
+            <div key = {item.idx}>
+
+                <div className='itemname'>
+                <p>{item.cos_name}</p>
+                </div>
+
+
+                <div className='img'>
+                <img src ={item.cos_img_src}/>
+                </div>
+
+                <div className='iteminfo'>
+
+                <div className='brand'>
+                <img src = {item.brand_img_src}/>
+                <p>{item.brand_name}</p>
+                </div>
+
+                <p>{item.cos_name}</p>
+                <p>{item.grade}</p><p>({item.grade_count})</p>
+                <p>{item.price}원</p><p>{item.vol}ml</p>
+                </div>
+
+
+                </div>
+        ))}
+
+    </div>
+   
   )
 }
 
