e565030324884991f12f1fb6fa95a981de4a731f
ddydyyy <dy240319@gmail.com>
2024-08-04 23:22:29 +0900

    0804

---
+++ old_/src/components/customswiper/CustomSwiper.jsx
@@ -0,0 +1,52 @@
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination, A11y, Autoplay } from 'swiper/modules';
+// Import Swiper styles
+import 'swiper/css';
+import { useEffect, useState } from 'react';
+import Star from '../../img/별.png'
+
+const CustomSwiper = ({ list }) => {
+    console.log(list)
+    return (
+        <Swiper
+            modules={[Autoplay]}
+            spaceBetween={2000}
+            slidesPerView={1}
+            onSlideChange={() => console.log('slide change')}
+            onSwiper={(swiper) => console.log(swiper)}
+            pagination={{ clickable: true }}
+            navigation
+            //   direction='vertical'
+            
+            loop={true}
+            speed={400}
+            autoplay={{ delay: 1000, disableOnInteraction: false }}
+            
+        >
+            {list.length > 0 && list.map((item, i)=>{
+                return (
+                <SwiperSlide key={i}>
+                    <img src={item.cos_img_src} alt="" />   
+                    <div className='cos_conater'>
+                        <p>
+                            {item.brand_name}
+                        </p>
+                        <p>{item.cos_name}</p>
+                        <p>{"정가 " + item.price + "원 / " + item.vol + "ml"}</p>
+                        <div className='flex_col'>
+                            <img className='star' src={Star} alt="" />
+                            <p>{"   "+item.grade+" ("+item.grade_count+")"}</p>
+                        </div>
+                    </div> 
+                </SwiperSlide>
+                )
+            })}
+            
+
+
+        </Swiper>
+    );
+};
+
+export default CustomSwiper;
---
+++ old_/src/util/util.js
@@ -0,0 +1,43 @@
+import axios from 'axios'
+
+export function sendGet(url, func = null, data=null){
+    
+    axios
+        .get(url, {
+            data : data,
+        })
+        .then(res => {
+            
+            if(func != null)
+            {
+                func(res.data)
+            }
+        })
+}
+
+export function sendPost(url, func=null, data=null)
+{
+    axios
+        .post(url, {
+            data : data
+        })
+        .then(res => {
+            // func(res.data)
+        })
+        
+}
+
+export function sendDel(url, func=null, data=null)
+{
+    axios
+        .delete(url, {
+            data : data
+        })
+        .then(res => {
+            // func(res.data)
+        })
+}
+
+export const URL = "http://192.168.219.111:5001";
+
+
