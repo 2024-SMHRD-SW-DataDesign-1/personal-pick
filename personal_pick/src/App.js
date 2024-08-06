3e182a0dc8a4aad7184079d84ce3ec1f53796a34
승진 <dydrkaan123@naver.com>
2024-08-06 14:45:42 +0900

    승진 푸쉬할게요

--- personal_pick/src/App.js
+++ personal_pick/src/App.js
@@ -18,7 +18,7 @@ function App() {
                     <Route path='/login' element={<Login />}></Route>
                     <Route path='/mypage' element={<Mypage />}></Route>
                     <Route path='/search' element={<Search />}></Route>
-                    <Route path='/detailinfo' element={<Detailinfo />}></Route>
+                    <Route path='/detailinfo/:idx' element={<Detailinfo />}></Route>
                 </Routes>
             </div>
         </BrowserRouter>
