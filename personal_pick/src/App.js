8fbce8991bdcdbbd7f522043a8afb7c7d7f49e5b
승진 <dydrkaan123@naver.com>
2024-08-06 14:51:30 +0900

    승진 다시 푸쉬

--- personal_pick/src/App.js
+++ personal_pick/src/App.js
@@ -17,20 +17,6 @@ const store = createStore(personalReducer)
 
 function App() {
     return (
-<<<<<<< HEAD
-        <BrowserRouter>
-            <div className="App">
-                <Routes>
-                    <Route path='/' element={<Home />}></Route>
-                    <Route path='/join' element={<Join />}></Route>
-                    <Route path='/login' element={<Login />}></Route>
-                    <Route path='/mypage' element={<Mypage />}></Route>
-                    <Route path='/search' element={<Search />}></Route>
-                    <Route path='/detailinfo/:idx' element={<Detailinfo />}></Route>
-                </Routes>
-            </div>
-        </BrowserRouter>
-=======
         <Provider store={store}>
             <BrowserRouter>
                 <div className="App">
@@ -40,12 +26,11 @@ function App() {
                         <Route path='/login' element={<Login />}></Route>
                         <Route path='/mypage' element={<Mypage />}></Route>
                         <Route path='/search' element={<Search />}></Route>
-                        <Route path='/detailinfo' element={<Detailinfo />}></Route>
+                        <Route path='/detailinfo:idx' element={<Detailinfo />}></Route>
                     </Routes>
                 </div>
             </BrowserRouter>
         </Provider>
->>>>>>> d1a1627a4ecae5300f28e927111d6e9f37e15362
     );
 }
 
