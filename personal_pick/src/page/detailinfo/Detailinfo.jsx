f6088da03b0a306aca5fd5306cf6fbddfe1fb548
승진 <dydrkaan123@naver.com>
2024-08-07 08:55:55 +0900

    승진 - 풀 받음

--- personal_pick/src/page/detailinfo/Detailinfo.jsx
+++ personal_pick/src/page/detailinfo/Detailinfo.jsx
@@ -4,6 +4,10 @@ import { useNavigate } from 'react-router-dom'
 import './Detailinfo.scss'
 import star from '../../img/별.png'
 import { useParams } from 'react-router-dom'
+import smile from '../../img/smile.png'
+import notsmile from '../../img/notSmile.png'
+import { FaStar } from 'react-icons/fa';
+
 
 const Detailinfo = () => {
     // 페이지 이동 함수
@@ -18,8 +22,19 @@ const Detailinfo = () => {
          sendGet(URL + "/DetailPage?idx="+idx , setData);
     },[]);
 
-    
+    const ARRAY = [0, 1, 2, 3, 4];
 
+    const [score, setScore] = useState([false, false, false, false, false]);
+    
+    const starScore = index => {
+        let star = [...score];
+        for (let i = 0; i < 5; i++) {
+          star[i] = i <= index ? true : false;
+        }
+        setScore(star);
+      };
+
+    
   return (
         <div id="wrapper" className='container'>
 
@@ -72,21 +87,71 @@ const Detailinfo = () => {
                         </div>
                         </div>
 
+                        <hr className='bar'/>
 
                         <div className='reviewinfo'>
-                        AI 가 분석한 리뷰
-                        </div>
+                        <span className='aireview'>AI 가 분석한 리뷰</span><br/>
+                        <img src ={smile} className='smile' width={40}/>
+                        <span className='like'>좋아요</span>
+                        <div className='likereview'>
+                            <span className='margintop1'>진정되는</span>
+                            <br/>
+                            <span className='margintop1'>쿨링되는</span>
+                            <br/>
+                            <span className='margintop1'>수분있는</span>
+                            <br/>
+                            <span className='margintop1'>모공관리되는</span>
+                            <br/>
+                            <span className='margintop1'>자극없는</span>
+                            <br/>
+                            <span className='margintop1'>향에 만족한</span>
+                            <br/>
+                            <span className='margintop1'>보습되는</span>
                         </div>
 
-                        {/* <h2>{item.brand_name}</h2>
-                        <p>{item.cos_name}</p>
-                        <p>{"정가 " + item.price + "원 / " + item.vol + "ml"}</p>
-                        <div className='flex_col'>
-                            <p>{"   " + item.grade + " (" + item.grade_count + ")"}</p>
-                        </div> */}
-                    </div>
-                    
-                    
+                        <div className='dislike'>
+                        <img src ={notsmile} className='smile' width={40}/>
+                        <span className='like'>아쉬워요</span>
+                            <span className='margintop2'>가루날림이 있는</span>
+                            <br/>
+                            <span className='margintop2'>마르는</span>
+                            <br/>
+                            <span className='margintop2'>흘러내리는</span>
+                            <br/>
+                            <span className='margintop2'>잔여물이 남는</span>
+                            <br/>
+                            <span className='margintop2'>노폐물 제거 안되는</span>
+                            <br/>
+                            <span className='margintop2'>가려운</span>
+                            <br/>
+                            <span className='margintop2'>내장 도구가 안좋은</span>
+                        </div>
+
+                        <hr className='bar2'/>
+
+                        <div className='review'>
+                            리뷰
+                        <div className='reviewcount'>
+                            1432
+                            </div>
+                            </div>
+
+                        <div className='reviewrate'>
+                            평점
+                            </div>
+                        <div className='reviewratestar'>
+                        {ARRAY.map((el, index) => (
+                                <FaStar
+                                    key={index}
+                                    size="14"
+                                ></FaStar>
+                                ))}
+                        </div>
+
+
+                        </div>
+                        </div>
+                    </div> 
                 ))
             ) : (
             // 데이터를 불러오는데 실패하면 실행
