6a00307f06668578f89ebb4d6d2fee809a8e4d6d
jw <jw39@gmail.com>
2024-08-07 13:02:59 +0900

    지원 커밋

--- personal_pick/backend/MainServer.py
+++ personal_pick/backend/MainServer.py
@@ -7,6 +7,7 @@ from ppDetail import ppDetailPage
 from join import ppJoin
 from ppLogin import ppLogin
 from ppSearch import ppSearch
+from ppSearchObject import ppSearchObject
 
 app = Flask(__name__, static_folder='../build', static_url_path='/')
 CORS(app) 
@@ -19,6 +20,7 @@ api.add_resource(ppDetailPage, '/DetailPage')
 api.add_resource(ppJoin, '/JoinPage')
 api.add_resource(ppLogin, '/LoginPage')
 api.add_resource(ppSearch, '/SearchPage')
+api.add_resource(ppSearchObject, '/SearchObject')
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5001, debug=True)
--- personal_pick/backend/db_utils.py
+++ personal_pick/backend/db_utils.py
@@ -53,3 +53,23 @@ def PostQuery(sql = None, data = None):
         return 201
     except Exception as e:
         return 500
+    
+def postQuery1(sql = None, data = None):
+    # MySQL 데이터베이스 연결
+    db = pymysql.connect(
+        host='127.0.0.1',
+        user='root',
+        password='1234',
+        db='recipe', 
+        charset='utf8mb4'
+    )
+    
+    # 데이터에 접근
+    cursor = db.cursor()
+
+    cursor.execute(sql,data)
+    
+    # DB 연결 종료
+    db.commit()
+    db.close()
+    return data
