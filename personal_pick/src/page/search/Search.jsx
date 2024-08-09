67f5cfef29466a2fdcf4e587574cd35992ac70a1
오세원 <energizer710@naver.com>
2024-08-09 14:28:39 +0900

    세원커밋

--- personal_pick/src/page/search/Search.jsx
+++ personal_pick/src/page/search/Search.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import InputBox from '../../components/inputbox/InputBox';
-import { sendGet, sendPost, URL } from '../../util/util';
+import { sendDel, sendGet, sendPost, URL } from '../../util/util';
 import './Search.scss';
 import Star from '../../img/별.png';
 import Back from '../../img/뒤로가기.png';
@@ -40,22 +40,29 @@ const Search = () => {
     useEffect(() => {
         // 검색어 전체 리스트 형식으로 가져오는 게 아니라 단어 객체 하나만 가져온다는 것
         sendGet(URL + "/SearchPage?value="+inputvalue, showConsole);
-    }, [inputvalue]); // 빈 배열을 두 번째 인자로 전달하여 마운트 시 한 번만 실행
+    }, [inputvalue]); 
 
 
     // 내가 찾고 싶은 제품 단어를 검색했을 때 나타나는 최근 검색어
     // searchHistory : 최근 검색어를 저장할 상태 변수(최근 검색어를 저장하는 배열)
     const [searchHistory, setSearchHistory] = useState([]);
 
-
+    // 굳이 sendDel을 만들지 않아도 sendPost로 내가 검색한 최근검새어 리스트(ex.5개)를 보낼 것이고
+    // X 버튼을 onClick 했을 때에 남은 리스트(ex.4개)도 sendPost로 보내기때문에 sendDel를 만들 필요 없음
     useEffect(()=>{
+        console.log(searchHistory);        
         sendPost(URL + "/SearchList", null, searchHistory);
     },[searchHistory]); // 빈배열 안에 searchHistory(최근 검색어)가 있는 경우는 최근검색어를 검색하고 화면에 나왔을 때 렌더링하겠다는 뜻
     
+    // 절대 주석 풀지 말 것!!!!!!!!!!!!
+    // useEffect(() => {
+    //     sendDel(URL + "/SearchList?value=", searchHistory)
+    // },[searchHistory])
+
     
     // 검색어를 추가하는 함수
     // inputbox에서 검색어를 searchadd로 받아옴 
-    const searchAdd = (searchValue) => {  // 새로운 검색어, 사용자가 검색을 수행할 때 handleSearch 함수로 전달
+    const searchAdd = (searchValue) => {  // 새로운 검색어
         setInputvalue(searchValue)
         // 이전 검색어 리스트에 새로운 검색어를 배열 끝에 추가
         // 5개 과거(이전) 데이터 있는데 + 현재(최신) 데이터 1개 => 총 6개 배열 나옴
