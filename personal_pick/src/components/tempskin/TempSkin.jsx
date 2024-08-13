c494127dcd24a893ebe5528e963e5fc939ae8f45
sunford <p01097220945@gmail.com>
2024-08-13 15:51:53 +0900

    상현 0813 tempSkin 완료 ver0.2

--- personal_pick/src/components/tempskin/TempSkin.jsx
+++ personal_pick/src/components/tempskin/TempSkin.jsx
@@ -1,18 +1,76 @@
 import React from 'react'
 import './TempSkin.scss'
 
+let dic = {
+    count : 3,
+    value : "피부 보습"
+}
 
-const TempSkin = ( {dic} ) => {
+let skinList = [
+    {
+        count : 3,
+        value : "피부 보습"
+    },{
+        count : 3,
+        value : "피부 보습"
+    },{
+        count : 3,
+        value : "피부 보습"
+    },{
+        count : 3,
+        value : "피부 보습"
+    },{
+        count : 3,
+        value : "피부 보습"
+    },{
+        count : 3,
+        value : "피부 보습"
+    },{
+        count : 3,
+        value : "피부 보습"
+    },{
+        count : 3,
+        value : "피부 보습"
+    },{
+        count : 3,
+        value : "피부 보습"
+    },{
+        count : 3,
+        value : "피부 보습"
+    },{
+        count : 3,
+        value : "피부 보습"
+    },
+    {
+        count : 3,
+        value : "피부 보습"
+    },
+    {
+        count : 3,
+        value : "피부 보습"
+    }
+]
+
+const TempSkin = ( {list} ) => {
     return (
-        <div className='flex_row tempSkin_Container'>
-            <div>{dic.count + "개"}</div>
-            <div className='skinChart_back'>
-                <div className='skinChart' style={{height : dic.count*20+"%", backgroundColor:"rgb(145 209 204)"}}>
-
-                </div>
-            </div>
-            <div>{dic.value}</div>
+        <div className='temSkin_main'>
+            {skinList.map((dic,idx)=>{
+                return (
+                    <div >
+                        <div className='tempSkin_Container' key={idx}>
+                            <div>{dic.count + "개"}</div>
+                            <div className='skinChart_back'>
+                                <div className='skinChart' style={{height : dic.count*20+"%", backgroundColor:"rgb(145 209 204)"}}>
+                
+                                </div>
+                            </div>
+                            <div className='tempskin_Title'>{dic.value}</div>
+                        </div>
+                    </div>
+                )
+            })}
         </div>
+
     )
 }
 
