f865020e0c32a3c8ba27a13815e59515c721bb18
sunford <p01097220945@gmail.com>
2024-08-13 15:19:54 +0900

    상현 0813 tempSkin 작업완료 ver0.1

--- personal_pick/src/components/tempskin/TempSkin.jsx
+++ personal_pick/src/components/tempskin/TempSkin.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import './TempSkin.scss'
 
-const TempSkin = () => {
-  return (
-    <div>TempSkin</div>
-  )
+
+const TempSkin = ( {dic} ) => {
+    return (
+        <div className='flex_row tempSkin_Container'>
+            <div>{dic.count + "개"}</div>
+            <div className='skinChart_back'>
+                <div className='skinChart' style={{height : dic.count*20+"%", backgroundColor:"rgb(145 209 204)"}}>
+
+                </div>
+            </div>
+            <div>{dic.value}</div>
+        </div>
+    )
 }
 
 export default TempSkin
