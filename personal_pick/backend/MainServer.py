00d54075799e1894039a15bf6afe3d6d4b9fae49
jw <jw39@gmail.com>
2024-08-13 18:09:57 +0900

    지원 08/13 푸터 수정 ver 0.4

--- personal_pick/backend/MainServer.py
+++ personal_pick/backend/MainServer.py
@@ -3,7 +3,7 @@ from flask_restx import Api
 from flask_cors import CORS
 # from hello import test
 from ppMain import ppMainPage, ppCategorySel
-from ppDetail import ppDetailPage, ppGetReveiw, ppScoreAvg, ppScoreCnt
+from ppDetail import ppDetailPage, ppGetReveiw, ppScoreAvg, ppScoreCnt, ppReviewCnt
 from join import ppJoin
 from ppLogin import ppLogin
 from ppSearch import ppSearch, ppSearchList
@@ -23,8 +23,9 @@ api.add_resource(ppSearch, '/SearchPage')
 api.add_resource(ppSearchList, '/SearchList')
 api.add_resource(ppCategorySel, '/CategorySel')
 api.add_resource(ppGetReveiw, '/ReviewPage')
-api.add_resource(ppScoreAvg, '/ScoreAvgPage')
-api.add_resource(ppScoreCnt, '/ScoreCntPage')
+api.add_resource(ppScoreAvg, '/ScoreAvg')
+api.add_resource(ppScoreCnt, '/ScoreCnt')
+api.add_resource(ppReviewCnt, '/ReviewCnt')
 
 api.add_resource(testJoin, '/TestJoin')
 api.add_resource(testLogin, '/TestLogin')
